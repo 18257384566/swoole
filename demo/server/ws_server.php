90894dc05509a3838aed99b8acbb2c071cb62405
sy <1404341518@qq.com>
2019-10-17 18:20:27 +0800

    test

--- demo/server/ws_server.php
+++ demo/server/ws_server.php
@@ -1,16 +1,16 @@
 <?php
 
-$server = new swoole_websocket_server('0.0.0.0', 8812);
+$server = new swoole_websocket_server('0.0.0.0', 9112);
 
 //监听websocket连接打开事件
 $server->on('open', function (Swoole\WebSocket\Server $server, $request) {
-    echo "server: handshake success with fd{$request->fd}\n";
+    echo "server: handshake success with fd:{$request->fd}\n";
 });
 
 //监听websocket消息事件
 $server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
     echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-    $server->push($frame->fd, "push server");
+    $server->push($frame->fd, "xsy:push server");
 });
 
 $server->on('close', function ($ser, $fd) {
