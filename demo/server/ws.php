7cd602444b854d3404a1639f9cb49ca77bed3405
sy <1404341518@qq.com>
2019-10-21 16:47:23 +0800

    test

--- demo/server/ws.php
+++ demo/server/ws.php
@@ -18,6 +18,8 @@ class Ws{
 
         $this->ws->on("open", [$this, 'onOpen']);
         $this->ws->on("message", [$this, 'onMessage']);
+        $this->ws->on("task", [$this, 'onTask']);
+        $this->ws->on("finish", [$this, 'onFinish']);
         $this->ws->on("close", [$this, 'onClose']);
 
         $this->ws->start();
@@ -31,7 +33,25 @@ class Ws{
     //监听ws消息事件
     public function onMessage($ws, $frame){
         echo "ser-push-message:{$frame->data}\n";
+        //do 10s
+        $data = [
+            'task' => 1,
+            'fd' => $frame->fd,
+        ];
+        $ws->task($data);
         $ws->push($frame->fd, "".date('Y-m-d H:i:s'));
+    }
+
+    public function onTask($serv, $taskId, $workerId, $data){
+        var_dump($data);
+        //模拟耗时场景10s
+        sleep(10);
+        return 'task finish';
+    }
+
+    public function onFinish($serv, $taskId, $data){
+        echo "taskId:{$taskId}";
+        echo "finish-task-sucess:{$data}";
     }
 
     //关闭
