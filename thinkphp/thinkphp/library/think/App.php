185eb2bb4b92addd107db4b3c70abed164d16e99
sy <1404341518@qq.com>
2019-10-23 17:15:24 +0800

    test

--- thinkphp/thinkphp/library/think/App.php
+++ thinkphp/thinkphp/library/think/App.php
@@ -621,7 +621,7 @@ class App
      */
     public static function routeCheck($request, array $config)
     {
-        $path   = $request->path();
+        $path   = $request->path(); echo "$path\n";
         $depr   = $config['pathinfo_depr'];
         $result = false;
 
