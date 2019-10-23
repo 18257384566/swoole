52ca358e07972cfe771f08d519e94c1d00d35c88
sy <1404341518@qq.com>
2019-10-23 16:01:25 +0800

    test

--- thinkphp/server/http_server.php
+++ thinkphp/server/http_server.php
@@ -13,14 +13,38 @@ $http->on('WorkerStart', function(swoole_server $server, $worker_id){
     // 定义应用目录
     define('APP_PATH', __DIR__ . '/../application/');
     //加载框架里的文件
-//    require __DIR__ . '/../thinkphp/base.php';
-    require __DIR__ . '/../thinkphp/start.php';
+    require __DIR__ . '/../thinkphp/base.php';
+//    require __DIR__ . '/../thinkphp/start.php';
 });
 
 
 $http->on('request', function($request, $response){ //$request:接受信息 $response:发送客户端
-        $response->cookie('name','neirong',time() + 1800);
-        $response->end("lalal".json_encode($request->get));
+    if(isset($request->server)){
+        foreach ($request->server as $k => $v){
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+    if(isset($request->header)){
+        foreach ($request->header as $k => $v){
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+    if(isset($request->get)){
+        foreach ($request->get as $k => $v){
+            $_GET[$k] = $v;
+        }
+    }
+    if(isset($request->post)){
+        foreach ($request->post as $k => $v){
+            $_POST[$k] = $v;
+        }
+    }
+
+    // 2. 执行应用
+    \think\App::run()->send();
+
+    $response->cookie('name','neirong',time() + 1800);
+    $response->end("lalal".json_encode($request->get));
 });
 
 $http->start();
