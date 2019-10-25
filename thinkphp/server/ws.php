8f6f157306d00eea3b15664df98dced77f7e6241
sy <1404341518@qq.com>
2019-10-25 18:31:57 +0800

    test

--- thinkphp/server/ws.php
+++ thinkphp/server/ws.php
@@ -71,7 +71,7 @@ class Ws{
         }
 
         $_FILES = [];
-        if(isset($request->post)){
+        if(isset($request->files)){
             foreach ($request->files as $k => $v){
                 $_FILES[$k] = $v;
             }
