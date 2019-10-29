9674423f902afe0384ef5423d0184b519f2df903
sy <1404341518@qq.com>
2019-10-29 13:29:37 +0800

    test

--- thinkphp/application/admin/controller/Live.php
+++ thinkphp/application/admin/controller/Live.php
@@ -5,6 +5,8 @@ class Live
 {
     public function push(){
         var_dump($_GET);
+        echo '<br>';
+        var_dump($_POST);
 
         //入库，整理数据push到前端
         $_POST['http_server']->push(2,'lalalalalla');
