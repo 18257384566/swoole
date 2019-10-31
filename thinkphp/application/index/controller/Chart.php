bd6a4bfe0ee3e4bb0b7260447b76a246c1529516
sy <1404341518@qq.com>
2019-10-31 17:37:50 +0800

    test

--- thinkphp/application/index/controller/Chart.php
+++ thinkphp/application/index/controller/Chart.php
@@ -6,6 +6,8 @@ class Chart
     public function index()
     {
         echo 'chart';
+        var_dump($_POST['http_server']);
+        echo '111';
         foreach ($_POST['http_server']->ports[1]->connections as $fd){
             $_POST['http_server']->push($fd, $fd);
         }
--- thinkphp/server/ws.php
+++ thinkphp/server/ws.php
@@ -101,7 +101,7 @@ class Ws{
 
     //监听ws连接事件
     public function onOpen($ws, $requst){
-
+        var_dump($ws);
         var_dump($requst->fd);
     }
 
