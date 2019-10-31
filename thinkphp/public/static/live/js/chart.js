d84f7bdd3f1d6440751f51ebd5079a20fb9ec5c5
sy <1404341518@qq.com>
2019-10-31 16:28:02 +0800

    test

---
+++ thinkphp/public/static/live/js/chart.js
@@ -0,0 +1,26 @@
+var wsurl = 'ws://118.31.109.21:9812';
+
+var websocket = new WebSocket(wsurl);
+
+//实例对象的onopen属性
+websocket.onopen = function(evt){
+    websocket.send('send:hello');
+    console.log("conected-swoole-success");
+}
+
+//实例化 onmessage
+websocket.onmessage = function(evt){
+    console.log("web-server-return-data:" + evt.data);
+}
+
+websocket.onclose = function(evt){
+    console.log("close");
+}
+
+websocket.onerror = function(evt, e){
+    console.log("error:" + evt.data);
+}
+
+function push(){
+
+}
