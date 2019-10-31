d327b76b832a2856f6f854a2885b4a07bb1adaa9
sy <1404341518@qq.com>
2019-10-31 15:04:42 +0800

    test

---
+++ thinkphp/application/index/controller/Chart.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\index\controller;
+
+class Chart
+{
+    public function index()
+    {
+        echo 'chart';
+    }
+
+
+}
---
+++ thinkphp/public/static/live/js/chart-push.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    $('#discuss-box').keydown(function(event){
+        if(event.KeyCode == 13){//回车事件
+            var text = $(this).val();
+            var url = 'http://118.31.109.21:9111/?s=index/chart/index';
+            var data = {'content':text, 'game_id':1};
+
+            $.post(url,data,function(result){
+                //todo
+                $(this),val('');
+            },'json');
+        }
+    });
+
+});
