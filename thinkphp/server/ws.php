598e4c9141f64f4e3034ba8acccd3fc104c6dded
sy <1404341518@qq.com>
2019-11-04 11:52:16 +0800

    test

--- thinkphp/server/ws.php
+++ thinkphp/server/ws.php
@@ -34,9 +34,52 @@ class Ws{
         $this->ws->on("finish", [$this, 'onFinish']);
         $this->ws->on("close", [$this, 'onClose']);
 
-        $this->ws->on('request', function($request, $response) {
-            var_dump('111');
+        $this->ws->on('request', function($request, $response){ //$request:接受信息 $response:发送客户端
+            var_dump('222');
+            if(!empty($_GET)){
+                unset($_GET);
+            }
+            if(!empty($_POST)){
+                unset($_POST);
+            }
 
+//var_dump($request->server);
+            if(isset($request->server)){
+                foreach ($request->server as $k => $v){
+                    $_SERVER[strtoupper($k)] = $v;
+                }
+            }
+            if(isset($request->header)){
+                foreach ($request->header as $k => $v){
+                    $_SERVER[strtoupper($k)] = $v;
+                }
+            }
+            if(isset($request->get)){
+                foreach ($request->get as $k => $v){
+                    $_GET[$k] = $v;
+                }
+            }
+            if(isset($request->post)){
+                foreach ($request->post as $k => $v){
+                    $_POST[$k] = $v;
+                }
+            }
+
+            // 2. 执行应用
+            ob_start();
+            try{
+                \think\App::run()->send();
+            }catch (\Exception $e){
+                // todo
+            }
+//    echo '-action-:'.request()->action().PHP_EOL;
+            $res = ob_get_contents(); //将缓存赋值给变量
+            ob_end_clean();
+
+//    $http->close();
+
+            $response->cookie('name','neirong',time() + 1800);
+            $response->end($res);
         });
 
         $this->ws->start();
