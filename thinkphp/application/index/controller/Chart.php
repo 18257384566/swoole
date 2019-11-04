e2105d0ea61959117566768a8cd259a058753321
sy <1404341518@qq.com>
2019-11-04 18:15:54 +0800

    v

--- thinkphp/application/index/controller/Chart.php
+++ thinkphp/application/index/controller/Chart.php
@@ -5,13 +5,12 @@ class Chart
 {
     public function index()
     {
-        echo 'chart';
+        var_dump($_GET);
 
         foreach ($_POST['http_server']->ports[1]->connections as $fd){
             $_POST['http_server']->push($fd, $fd);
         }
 
-        echo 'ee';
     }
 
 
