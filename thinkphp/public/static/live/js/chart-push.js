d4a4abea17665f8645403d20b09bc651f48fceb8
sy <1404341518@qq.com>
2019-11-04 12:16:33 +0800

    test

--- thinkphp/public/static/live/js/chart-push.js
+++ thinkphp/public/static/live/js/chart-push.js
@@ -3,7 +3,7 @@ $(function(){
     $('#discuss-box').keydown(function(event){
         if(event.keyCode == 13){//回车事件
             var text = $(this).val();
-            var url = 'http://118.31.109.21:9112/?s=index/chart/index';
+            var url = 'http://118.31.109.21:9111/?s=index/chart/index';
             var data = {'content':text, 'game_id':1};
 
             $.post(url,data,function(result){
