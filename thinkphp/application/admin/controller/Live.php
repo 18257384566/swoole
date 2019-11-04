9f3e34d374b4ae0d937f50e2f60d1900dc086dd8
sy <1404341518@qq.com>
2019-11-04 15:04:50 +0800

    test

--- thinkphp/application/admin/controller/Live.php
+++ thinkphp/application/admin/controller/Live.php
@@ -4,15 +4,12 @@ namespace app\admin\controller;
 class Live
 {
     public function push(){
-        var_dump($_GET);
-        echo '<br>';
-        var_dump($_POST);
 
         //入库，整理数据push到前端
         $ws = $_POST['http_server'];
         foreach($ws->connections as $fd)
         {
-            $ws->send($fd, "xsy-hello");
+            $ws->send($fd, "xsy-hello-live");
         }
     }
 
