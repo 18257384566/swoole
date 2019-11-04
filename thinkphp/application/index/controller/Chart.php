b89668ec36d4c989d1b2cbc4923b37aaa9121fc7
sy <1404341518@qq.com>
2019-11-04 12:29:24 +0800

    test

--- thinkphp/application/index/controller/Chart.php
+++ thinkphp/application/index/controller/Chart.php
@@ -7,12 +7,11 @@ class Chart
     {
         echo 'chart';
 
-        var_dump($_POST['http_server']);
-        echo '111';
-
         foreach ($_POST['http_server']->ports[1]->connections as $fd){
             $_POST['http_server']->push($fd, $fd);
         }
+
+        echo 'ee';
     }
 
 
