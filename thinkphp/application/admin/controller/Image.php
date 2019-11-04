0f7bf1a0eee9378ba86c0987e8cd0bc342cecebb
sy <1404341518@qq.com>
2019-11-04 14:39:32 +0800

    test

--- thinkphp/application/admin/controller/Image.php
+++ thinkphp/application/admin/controller/Image.php
@@ -1,12 +1,20 @@
 <?php
 namespace app\admin\controller;
 
+use Phinx\Util\Util;
+ 
 class Image
 {
     public function index(){
-        echo "333\n";
-        var_dump($_GET); echo '222';
-        var_dump($_FILES);echo '444';
+        $files = request()->file('file');
+        $info = $files->move('../public/static/upload');
+        if($info){
+            $data = [
+                'image' => 'http://118.31.109.21:8811/'.$info->getSaveName(),
+            ];
+            return Util::show(config('code.success'),'ok',$data);
+        }
+        return Util::show(config('code.error '),'error');
     }
 
 
