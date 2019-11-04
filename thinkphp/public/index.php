073f513d8c3c7162dfb1be92e9c0684fe3c38e64
sy <1404341518@qq.com>
2019-11-04 13:38:45 +0800

    test

--- thinkphp/public/index.php
+++ thinkphp/public/index.php
@@ -10,13 +10,19 @@
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
-header('Access-Control-Allow-Origin: *');
+
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
 
+//跨域
+header('Access-Control-Allow-Headers:c-token,device-type,au-token,lang,Origin, Content-Type, Cookie, Accept');
+header('Access-Control-Allow-Methods:GET,POST,OPTIONS');
+header('Access-Control-Allow-Credentials:true');
+header('Access-Control-Expose-Headers:c-token,device-type,au-token,lang');
+//header("Access-Control-Allow-Origin:$url");
 
 
 
