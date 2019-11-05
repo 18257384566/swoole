d860ef1d5e976a7718fe086eb313fc6bd730c6f2
sy <1404341518@qq.com>
2019-11-05 14:33:52 +0800

    v

--- thinkphp/application/admin/controller/Live.php
+++ thinkphp/application/admin/controller/Live.php
@@ -6,14 +6,14 @@ class Live
     public function push(){
         var_dump($_GET);
 
-        $_POST['http_server']->push(3,'xsy-hello-live');
+//        $_POST['http_server']->push(3,'xsy-hello-live');
 
         //入库，整理数据push到前端
-//        $ws = $_POST['http_server'];
-//        foreach($ws->connections as $fd)
-//        {
-//            $ws->send($fd, "xsy-hello-live");
-//        }
+        $ws = $_POST['http_server']->ports[0];
+        foreach($ws->connections as $fd)
+        {
+            $ws->send($fd, "xsy-hello-live");
+        }
     }
 
 
