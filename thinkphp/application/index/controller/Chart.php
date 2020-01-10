316422454db3c2e51c382ffe1d7b6dd448af4601
sy <1404341518@qq.com>
2020-01-10 17:29:21 +0800

    test

--- thinkphp/application/index/controller/Chart.php
+++ thinkphp/application/index/controller/Chart.php
@@ -24,6 +24,7 @@ class Chart
         //给所有用户推送消息
         var_dump($_POST['http_server']->ports[1]->connections);
         var_dump($_POST['http_server']->ports[0]->connections);
+        var_dump($_POST['http_server']->ports[2]->connections);
         $a = [
             0 => 5,
             1 => 2,
